f6968dc78ff6feb1b0626ccbe48c9f755664fe34
educd2017 <44037870+educd2017@users.noreply.github.com>
2025-07-03 13:11:31 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "aliraafatblog-cache-v1";
+const urlsToCache = [
+  "/",
+   "/p/artical.html",
+  
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
